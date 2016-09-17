43ee55a574921e20400ca01e8bf4acb4b2f7385e
Zachery Miranda <zmiranda@mit.edu>
2016-09-17 14:25:00 -0400

    obama speeches

--- word_counter.py
+++ word_counter.py
@@ -6,10 +6,14 @@ filename = 'speeches/dnc-2016-obama-prepared-remarks.txt'
 with open(filename) as f:
     passage = f.read()
 
+with open('stop_words.txt') as f:
+	stop_words = f.read().split()
+
 words = re.findall(r'\w+', passage)
 
-cap_words = [word.upper() for word in words]
+filtered_words = [word.lower() for word in words if word.lower() not in stop_words]
 
-word_counts = Counter(cap_words)
+
+word_counts = Counter(filtered_words)
 
 print word_counts.most_common(10)
